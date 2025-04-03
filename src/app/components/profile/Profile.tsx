b8a1d0cc55ccc07a5bdfd4b3a8f1433fd50671e7
mobin <mobin@ghasem.debian>
2025-04-03 08:09:26 +0330

    modified style

--- src/app/components/profile/Profile.tsx
+++ src/app/components/profile/Profile.tsx
@@ -2,7 +2,7 @@ import { CgProfile } from "react-icons/cg";
 import { RxHamburgerMenu } from "react-icons/rx";
 export default function Profile () {
     return <div className="flex">
-        <RxHamburgerMenu className="relative left-145 text-3xl"/>
-        <CgProfile className="relative left-150 text-3xl"/>
+        <RxHamburgerMenu className="relative left-175 text-3xl"/>
+        <CgProfile className="relative left-180 text-3xl"/>
     </div>
 }
